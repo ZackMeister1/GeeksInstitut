098fd56a0866003d1250805042933a265d1bff27
Doflamingo <zackwebmasters@gmail.com>
2026-02-16 15:22:03 +0000

    Add the folder

--- week_1/day_4/exercises/exercises_xp.js
+++ week_1/day_4/exercises/exercises_xp.js
@@ -1,9 +1,68 @@
 // ===== Exercise 1
+const colorsDiv = document.getElementById("colors");
+const board = document.getElementById("board");
+const clearBtn = document.getElementById("clear");
 
-// ===== Exercise 2
+const colors = ["red", "blue", "green", "yellow", "black", "purple"];
 
-// ===== Exercise 3
+let selectedColor = "black";
+let isMouseDown = false;
 
-// ===== Exercise 4
 
-// ===== Exercise ...
+
+/* Create color palette */
+colors.forEach(color => {
+  const colorBox = document.createElement("div");
+  colorBox.classList.add("color");
+  colorBox.style.backgroundColor = color;
+
+  colorBox.addEventListener("click", function() {
+    selectedColor = color;
+  });
+
+  colorsDiv.appendChild(colorBox);
+});
+
+
+
+/* Create grid */
+for (let i = 0; i < 256; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  board.appendChild(square);
+}
+
+
+/* Drawing logic */
+board.addEventListener("mousedown", function(e) {   
+
+  if (e.target.classList.contains("square")) {
+    isMouseDown = true;
+    e.target.style.backgroundColor = selectedColor;
+  }
+});
+
+board.addEventListener("mouseover", function(e) {
+  if (isMouseDown && e.target.classList.contains("square")) {
+    e.target.style.backgroundColor = selectedColor;
+  }
+});
+
+document.addEventListener("mouseup", function() {
+  isMouseDown = false;
+});
+
+
+/* Clear button */
+clearBtn.addEventListener("click", function() {   
+  const squares = document.querySelectorAll(".square");
+  squares.forEach(square => {
+    square.style.backgroundColor = "white";
+  });
+});
+
+
+
+
+
+
